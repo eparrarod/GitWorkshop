a5873106feb2f0bea230dbc61f92536d05c2f116
Osamu Dazai <osamu.dazai@bsd-fiction.com>
2023-10-28 20:10:57 -0500

    Add a greeting function to program.py

--- program.py
+++ program.py
@@ -1,4 +1,7 @@
+def greet(name):
+    return f'Hello, {name}!'
 print('Hello, Git!')
 print('This is a sample program for a Git workshop.')
 for i in range(5):
     print(f'Counting: {i}')
+print(greet('Git User'))
